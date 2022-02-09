72154767fe91f7e07098236e0e95ab3326e7076c
OLINYA-PAUL <olinyacpaul7849@gmail.com>
2022-02-09 04:35:22 +0100

    my second commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// let menuDropDown = document.getElementById('menuDropDown')
+// function menuBar(){
+//     menuDropDown.style.maxHeight = '1000px'
+// }
+// function menuDropDownToggle(){
+//     menuDropDown.style.maxHeight = '0px'
+// }
